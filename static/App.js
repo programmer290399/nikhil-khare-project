15b91bc2e152cc1aa9e8cd64311e0b8199f3097b
Saahil Ali <programmer290399@gmail.com>
2021-12-13 21:00:36 +0530

    Updated JS to include live app url

--- static/App.js
+++ static/App.js
@@ -28,7 +28,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
-    var url = "http://127.0.0.1:5000/predict_home_price"; 
+    var url = "https://price-prediction-system.herokuapp.com/predict_home_price"; 
   
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
@@ -44,7 +44,7 @@ function getBathValue() {
   
   function onPageLoad() {
     console.log( "document loaded" );
-     var url = "http://127.0.0.1:5000/get_location_names"; 
+     var url = "https://price-prediction-system.herokuapp.com/get_location_names"; 
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
